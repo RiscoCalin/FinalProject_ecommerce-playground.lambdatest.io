c390c8b59bace448ac1afbfb3c80e63176a0e613
Katya <riscocalin@gmail.com>
2024-02-06 19:58:31 +0200

    ioi ca mere'

--- src/test/java/BuyiRefrigeratorTests.java
+++ src/test/java/BuyiRefrigeratorTests.java
@@ -31,18 +31,24 @@ public class BuyiRefrigeratorTests extends BasePage {
         buyiRefrigeratorPage.ClickOnBuyNow();
         Duration timeout = Duration.ofSeconds(10); //comada pentru timp de asteptare
         WebDriverWait wait = new WebDriverWait(driver, timeout);
-        WebElement guestCheckoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[5]/div[1]/div/div/form/div/div[1]/div/div[1]/div/div[3]")));
+        WebElement guestCheckoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[5]/div[1]/div/div/form/div/div[1]/div/div[1]/div/div[3]")));//asteapta ca elementul sa fie vizibil
         guestCheckoutButton.click();// Click on Guest Checkout button
         buyiRefrigeratorPage.ClickOnLogIn();
         buyiRefrigeratorPage.typeEmailField("riscocalin@gmail.com");
         buyiRefrigeratorPage.typePasswordField("YkMx5JLQMEqn@cT");
         buyiRefrigeratorPage.ClickOnLogInButton();
+        WebElement firstName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#input-payment-lastname")));
         buyiRefrigeratorPage.typeFirstName("Test");
         buyiRefrigeratorPage.typeLastName("Test1");
         buyiRefrigeratorPage.typeAddress("Calea Mortilor");
+        WebElement city = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#input-payment-city")));
         buyiRefrigeratorPage.typeCity("Iasi");
         buyiRefrigeratorPage.typePostCode("123456");
         buyiRefrigeratorPage.typeComment("Cand il aduceti sa fie plin cu bere");
+        buyiRefrigeratorPage.ClickOnAgree();
+        buyiRefrigeratorPage.setMouseOverContinue();
+        buyiRefrigeratorPage.ClickOnContinue();
+
 
     }
 
